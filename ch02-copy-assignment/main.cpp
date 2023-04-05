dbcbfdd1586d0b87b717797582c94becc6300169
mitchcamza <192mitch88@gmail.com>
2023-04-05 16:40:40 +0200

    Add class definition with default constructor, copy constructor, overloaded assignment operator, and destructor

--- ch02-copy-assignment/main.cpp
+++ ch02-copy-assignment/main.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 
-using namespace std;
+
+class MyClass
+{
+private:
+    int* data;
+
+public:
+    MyClass()
+    {
+        std::cout << "Default constructor called" << std::endl;
+    }
+
+    MyClass(const MyClass& other)
+    {
+        std::cout << "Copy constructor called" << std::endl;
+        data = new int[1];
+        *data = *(other.data);
+    }
+
+    MyClass& operator=(const MyClass& other)
+    {
+        std::cout << "Assignment operator called" << std::endl;
+        if (this != &other)
+        {
+            delete[] data;
+            data = new int[1];
+            *data = *(other.data);
+        }
+        return *this;
+    }
+
+    ~MyClass()
+    {
+        std::cout << "Destructor called" << std::endl;
+        delete[] data;
+    }
+};
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    MyClass obj1;           // calls default constructor
+    MyClass obj2 = obj1;    // calls copy constructor
+    MyClass obj3;           // calls default constructor
+    obj3 = obj2;            // calls assignment operator
+
     return 0;
 }
